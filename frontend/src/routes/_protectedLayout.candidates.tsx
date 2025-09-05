1cfc84b8c0aa4d3ed9cad6b5cd5a5ff58061aa43
Egor Fedorov <fegor2004@gmail.com>
2025-09-05 10:25:14 +0300

    implement candidates page

--- frontend/src/routes/_protectedLayout.candidates.tsx
+++ frontend/src/routes/_protectedLayout.candidates.tsx
@@ -5,6 +5,8 @@ import type { CandidateRead } from "../api/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { useCandidatesTable } from "../components/candidates/table";
 import { DataTable } from "@/components/ui/data-table";
+import { Button } from "@/components/ui/button";
+import { PlusIcon, UploadIcon } from "lucide-react";
 
 export const Route = createFileRoute("/_protectedLayout/candidates")({
 	component: CandidatesPage,
@@ -13,5 +15,31 @@ export const Route = createFileRoute("/_protectedLayout/candidates")({
 function CandidatesPage() {
 	const candidates = useSuspenseQuery(candidatesQueryOptions());
 
-	return "Hi"
+	const table = useCandidatesTable(candidates.data ?? []);
+
+	return <div className="space-y-6">
+		<header className="flex flex-row justify-between items-center">
+			<div className="flex flex-col">
+				<h1 className="text-2xl font-bold mb-2">
+					Кандидаты
+				</h1>
+				<p className="text-muted-foreground">
+					Управление кандидатами и отслеживание процесса найма
+				</p>
+			</div>
+			<div className="flex flex-row gap-2">
+				<Button variant="outline">
+					<PlusIcon />
+					Создать
+				</Button>
+				<Button variant="outline">
+					<UploadIcon />
+					Загрузить резюме
+				</Button>
+			</div>
+		</header>
+		<main>
+			<DataTable table={table} />
+		</main>
+	</div>
 }
