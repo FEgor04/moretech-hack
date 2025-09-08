884568fec0f9b8689339fb993e3b2c44384624de
Egor Fedorov <fegor2004@gmail.com>
2025-09-08 10:49:33 +0300

    fix black issues

--- backend/alembic/versions/000019_merge_heads.py
+++ backend/alembic/versions/000019_merge_heads.py
@@ -9,7 +9,6 @@ Create Date: 2025-09-08 10:46:40.573357
 from typing import Sequence, Union
 
 
-
 revision: str = "000019"
 down_revision: Union[str, None] = ("000017", "000018")
 branch_labels: Union[str, Sequence[str], None] = None
